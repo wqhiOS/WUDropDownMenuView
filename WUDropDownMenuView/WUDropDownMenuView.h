2124250eb51ae50361ebf3ddd5cf83867311e31f
wuqihan <wuqihan123@163.com>
2015-10-20 18:30:13 +0800

    init

---
+++ WUDropDownMenuView/WUDropDownMenuView.h
@@ -0,0 +1,13 @@
+//
+//  WUDropDownMenuView.h
+//  WUDropDownMenuView
+//
+//  Created by wuqh on 15/10/20.
+//  Copyright © 2015年 吴启晗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WUDropDownMenuView : UIView
+
+@end
---
+++ WUDropDownMenuView/WUDropDownMenuView.m
@@ -0,0 +1,21 @@
+//
+//  WUDropDownMenuView.m
+//  WUDropDownMenuView
+//
+//  Created by wuqh on 15/10/20.
+//  Copyright © 2015年 吴启晗. All rights reserved.
+//
+
+#import "WUDropDownMenuView.h"
+
+@implementation WUDropDownMenuView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
